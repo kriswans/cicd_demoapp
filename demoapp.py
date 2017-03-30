6f5998dae55356dbb611f50da9e436c5ef731e22
root <kris@swannynet.com>
2017-03-30 20:00:17 +0000

    Added hello/universe to the application-again

--- demoapp.py
+++ demoapp.py
@@ -18,6 +18,8 @@ class HelloUniverse(Resource):
         text = "Hello Universe!"
         return text
 
+api.add_resource(HelloUniverse, '/hello/universe')
+
 if __name__ == '__main__':
     # Runn Flask
     app.run(debug=True, host='0.0.0.0', port=int("5000"))
